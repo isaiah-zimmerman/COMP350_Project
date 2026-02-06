5ef2e0590769d05e4686f851f7902b0ee1d31682
isaiah-zimmerman <zimmycrafterplaysminecraftpepc@gmail.com>
2026-02-06 13:39:50 -0500

    changed Driver

--- src/main/java/edu/gcc/comp350/prij/Course_Scheduler/Driver.java
+++ src/main/java/edu/gcc/comp350/prij/Course_Scheduler/Driver.java
@@ -4,5 +4,6 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello jars");
         System.out.println("jsdfkljksdj");
+        System.out.println("test");
     }
 }
