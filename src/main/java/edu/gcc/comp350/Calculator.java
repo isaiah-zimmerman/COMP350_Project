3cff3c76e13f2add6a2ada1ce2f9ba7177bee74e
isaiah-zimmerman <zimmycrafterplaysminecraftpepc@gmail.com>
2026-02-06 10:20:38 -0500

    deletions

--- src/main/java/edu/gcc/comp350/Calculator.java
+++
@@ -1,7 +0,0 @@
-package edu.gcc.comp350;
-
-public class Calculator {
-    public int add(int a, int b){
-        return a + b;
-    }
-}
--- src/main/java/edu/gcc/comp350/Driver.java
+++
@@ -1,9 +0,0 @@
-package edu.gcc.comp350;
-
-public class Driver {
-    public static void main(String[] args) {
-        System.out.println("hello jar");
-        Calculator c = new Calculator();
-        System.out.println(c.add(5, 8));
-    }
-}
