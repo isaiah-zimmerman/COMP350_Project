644849a36cbc6f1d950ea3c59af889bfee9b99f0
Peter Brumbach <contact@peterbrumbach.com>
2026-02-09 13:23:26 -0500

    Test Commit

--- src/main/java/edu/gcc/comp350/prij/Course_Scheduler/Driver.java
+++ src/main/java/edu/gcc/comp350/prij/Course_Scheduler/Driver.java
@@ -3,7 +3,7 @@ package edu.gcc.comp350.prij.Course_Scheduler;
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello jars");
-        System.out.println("jsdfkljksdj");
+        System.out.println("jsdfklifbr3bvcoqjksdj");
         System.out.println("test");
     }
 }
